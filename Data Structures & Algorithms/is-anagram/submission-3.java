be9e8176f0a3bbaffd59ab596f4cc48923dab58c
Miguel Kalaw <163828790+Miguel2604@users.noreply.github.com>
2026-05-01 10:17:00 +0800

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,40 @@
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] firstString = s.toCharArray();
+        char[] secondString = t.toCharArray();
+
+        HashMap<Character, Integer> firstHash = new HashMap<>();
+        HashMap<Character, Integer> secondHash = new HashMap<>();
+
+        int firstCounter = 0;
+        int secondCounter = 0;
+        
+        if (firstString.length != secondString.length) {
+                    return false;
+                }
+
+        for(int i = 0; i < firstString.length; i++){
+                
+                if (firstHash.containsKey(firstString[i])) {
+                    firstCounter = firstHash.get(firstString[i]) + 1;
+                    firstHash.put(firstString[i], firstCounter);           
+            }
+                else{
+                    firstHash.put(firstString[i], 1);
+                }
+                if (secondHash.containsKey(secondString[i])) {
+                    secondCounter = secondHash.get(secondString[i]) + 1;
+                    secondHash.put(secondString[i], secondCounter);           
+            }
+                else{
+                    secondHash.put(secondString[i], 1);
+                }
+        }
+        if(firstHash.equals(secondHash)){
+            return true;
+        }
+        return false;
+    }
+} 
+
